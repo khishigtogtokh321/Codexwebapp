2e6d6e54c54ee54e3ef7146c195f822f2066f320
S.Khishigtogtokh <khishigtogtokh985@gmail.com>
2026-01-08 12:57:14 +0800

    suu bolon holimog shudnii tomyo

--- src/utils/toothHelpers.js
+++ src/utils/toothHelpers.js
@@ -5,7 +5,7 @@
 
 // FDI Tooth numbering system
 export const FDI_NOTATION = {
-  UPPER_RIGHT: [18, 17, 16, 15, 14, 13, 12, 11],
+  UPPER_RIGHT: [11, 12, 13, 14, 15, 16, 17, 18],
   UPPER_LEFT: [21, 22, 23, 24, 25, 26, 27, 28],
   LOWER_LEFT: [31, 32, 33, 34, 35, 36, 37, 38],
   LOWER_RIGHT: [41, 42, 43, 44, 45, 46, 47, 48],
